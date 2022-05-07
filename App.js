fa5188ed7c0e6b1afe3e7684b73d4f83e224f9ca
pr0gmtc <pr0gmtc@tutanota.com>
2022-05-07 09:13:28 +0200

    feat: SideDrawer

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
 import * as React from 'react';
+import 'react-native-gesture-handler';
 import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import Map from './screens/Map'
 import SideDrawer from './components/SideDrawer'
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Button } from 'react-native-paper';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <PaperProvider>
-      <Map />
-      <SideDrawer />
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
