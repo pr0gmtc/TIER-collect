b865a99fb11af7a5c3674a8e74e0904fbe5f244c
Beksultan Kassiyenov <beksultan@DESKTOP-TI0TQU1.localdomain>
2022-05-07 20:39:40 +0200

    Stickers done

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as React from "react";
+import "react-native-gesture-handler";
+import MapView from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import Map from "./screens/Map";
+import SideDrawer from "./components/SideDrawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Button } from "react-native-paper";
+import Home from "./screens/Home";
+import Collections from "./screens/Collections";
+import BottomDrawer from "./components/BottomDrawer";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-      <View style={styles.container}>
-        <MapView style={styles.map} />
-      </View>
-  );
+    return (
+        <PaperProvider>
+            <NavigationContainer>
+                <Drawer.Navigator initialRouteName="Home">
+                    <Drawer.Screen name="Home" component={Home} />
+                    <Drawer.Screen name="Collections" component={Collections} />
+                    <Drawer.Screen name="Map" component={Map} />
+                </Drawer.Navigator>
+            </NavigationContainer>
+            <BottomDrawer onDrawerStateChange={() => console.log("Yo")}>
+                <Text>ADSFASDFA</Text>
+            </BottomDrawer>
+        </PaperProvider>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
+const styles = StyleSheet.create({});
---
+++ screens/Home.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+import { Button } from 'react-native-paper';
+
+export default function Home({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        </View>
+    );
+}
